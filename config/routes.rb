a5522f4bb2299a60f2f2458ef02d0c24b5f00ad6
kiki0718 <71147023+kiki0718@users.noreply.github.com>
2021-01-22 23:42:05 +0900

    顧客管理機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "customers#index"
-  resources :items, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
+  resources :customers, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
   end
 end
